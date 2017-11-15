d0da514bf26ab259706ba9ee0469a40442b2abcc
Nahid Hasan <nahidh527@gmail.com>
2017-11-15 12:04:35 +0600

    Push Fake Data

--- database/factories/ModelProductFactory.php
+++ database/factories/ModelProductFactory.php
@@ -1,9 +1,15 @@
 <?php
 
 use Faker\Generator as Faker;
+use Ramsey\Uuid\Uuid;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
+        'id' => Uuid:: uuid4(),
+        'name' => $faker->word,
+        'detail'=> $faker->paragraph,
+        'price' => $faker->numberBetween(100,1000),
+        'stock' => $faker->randomDigit,
+        'discount'=> $faker->numberBetween(2,30),
     ];
 });
--- database/factories/ModelReviewFactory.php
+++ database/factories/ModelReviewFactory.php
@@ -1,9 +1,17 @@
 <?php
 
 use Faker\Generator as Faker;
+use Ramsey\Uuid\Uuid;
+use App\Model\Product;
 
 $factory->define(App\Model\Review::class, function (Faker $faker) {
     return [
-        //
+        'id' => Uuid:: uuid4(),
+        'customer' => $faker->name,
+        'review'=> $faker->paragraph,
+        'star' => $faker->numberBetween(0,5),
+        'product_id' => function() {
+        	return Product::all()->random();
+        }
     ];
 });
